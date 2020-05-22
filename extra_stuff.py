18f8fc6049a6fc843258fe56325ac0e669e4729e
shayanng <52363545+shayanng@users.noreply.github.com>
2020-05-22 00:31:42 -0400

    added extra_stuff.py for extra or alternative codes (commented)

---
+++ extra_stuff.py
@@ -0,0 +1,13 @@
+
+# class ransactionID:
+#     def __init__(self, start_id):
+#         self._start_id = start_id
+#
+#     def __next__(self):
+#         pass
+
+
+# def transaction_ids(start_id):
+#     while True:
+#         start_id += 1
+#         yield start_id
--- tarnsactionID.py
+++ tarnsactionID.py
@@ -1,26 +1,14 @@
 import itertools
 
 
-# class ransactionID:
-#     def __init__(self, start_id):
-#         self._start_id = start_id
-#
-#     def __next__(self):
-#         pass
-
-
-# def transaction_ids(start_id):
-#     while True:
-#         start_id += 1
-#         yield start_id
-
-
 class Account:
     transaction_counter = itertools.count(100)
 
     def make_transaction(self):
         new_transaction_id = next(Account.transaction_counter)
         return new_transaction_id
+
+
 
 
 acc1 = Account()
